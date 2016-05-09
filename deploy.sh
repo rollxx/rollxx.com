2ebaa003582ed2dbab466f058b414ecfed50ee04
roll <roll@xx-2.home>
2016-05-09 20:24:34 +0200

    another fix

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ hugo # if using a theme, replace by `hugo -t <yourtheme>`
 # Go To Public folder
 cd public
 
-git config user.name "$GIT_NAME"
-git config user.email "$GIT_EMAIL"
+#git config user.name "$GIT_NAME"
+#git config user.email "$GIT_EMAIL"
 
 
 # Add changes to git.
